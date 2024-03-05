766e5348bc0d733b91d35ff3815da5a5a9ec57fd
Pauline-Oraro <hiltrapauline@gmail.com>
2024-03-05 21:12:43 +0300

    react files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* header section */} 
+      
+      <div className='header'>
+
+        <div className='logo'>
+          <a href='/'>
+            <h1>REACTFOLIO</h1>
+          </a>
+        </div>
+
+        <input className='side-menu' type='checkbox' id="side-menu" name='menu' />
+        <label htmlFor='side-menu' className='hamb'>
+          <span className='hamb-line'></span>
+        </label>
+
+        <nav className='nav'>
+          <ul className='menu'>
+            <li>
+              <a href="#home">HOME</a>
+            </li>
+            <li>
+              <a href="#services">SERVICES</a>
+            </li>
+            <li>
+              <a href="#skills">SKILLS</a>
+            </li>
+            <li>
+              <a href="#portfolio">PORTFOLIO</a>
+            </li>
+            <li>
+              <a href="#contact">CONTACT</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div class="contactContainer">
+          <h2>CONTACT</h2>
+        </div>
+
+      </div>
     </div>
   );
 }
